eae7e99ea27162ab30642addcd0243878209544f
Sam Warr <sam.warr.tx@gmail.com>
2025-11-24 17:32:47 -0700

    toolchain fixes pt2

--- platform.py
+++ platform.py
@@ -33,7 +33,8 @@ class W60xPlatform(PlatformBase):
             #self.packages["toolchain-gccarmnoneeabi"]["version"] = "~1.90201.0"
             #self.packages["toolchain-gccarmnoneeabi"]["version"] = "~1.40903.0"
             #self.packages["toolchain-gccarmnoneeabi"]["version"] = "~1.50201.0"
-            self.packages["toolchain-gccarmnoneeabi"]["version"] = "~1.140201.0"
+            self.packages["toolchain-gccarmnoneeabi"]["version"] = "~1.50401.190816"
+            #self.packages["toolchain-gccarmnoneeabi"]["version"] = "~1.140201.0"
             self.frameworks['arduino']['package'] = "framework-arduino-w60x"
             self.packages['framework-arduino-w60x']['optional'] = False
 
